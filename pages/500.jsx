ad836d3e9b6c1a8f81a3deb0c73f74d1484eaa81
LeeEugene1 <uwm1004@gmail.com>
2022-05-24 23:17:00 +0900

    errorpage,.env.development

---
+++ pages/500.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import ErrorHandle from 'src/components/layout/ErrorHandle'
+
+function Error505() {
+  return (
+    <div>
+      <ErrorHandle statusCode={505} />
+    </div>
+  )
+}
+
+export default Error505
