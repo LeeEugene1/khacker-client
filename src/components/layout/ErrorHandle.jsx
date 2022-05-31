ec03150bc9d3e5e0abdc287bf2f4b778c73f0b90
LeeEugene1 <uwm1004@gmail.com>
2022-05-31 23:54:52 +0900

    error message

--- src/components/layout/ErrorHandle.jsx
+++ src/components/layout/ErrorHandle.jsx
@@ -9,7 +9,7 @@ function ErrorHandle({ statusCode }) {
         <title>{statusCode} 페이지</title>
       </Head>
       <h3>{statusCode} 에러</h3>
-      <p>죄송합니다. 페이지를 찾을 수 없습니다.</p>
+      <p>죄송합니다. 페이지를 찾을 수 없습니다. 잠시후 다시 이용해주세요.</p>
       <Link href="/">홈으로 이동하기</Link>
     </section>
   )
